1a2237bd53441d19fca03a31ce63c1663b65bcfb
Simona Gorneanu <gorneanusimona@gmail.com>
2019-12-18 23:35:33 +0200

    inline-block

--- teme JS/tableX/tableX.js
+++ teme JS/tableX/tableX.js
@@ -1,9 +1,10 @@
 for (i = 1; i < 10; i++) {
+    document.write('<span style="display:inline-block"> <pre>');
     for (j = 1; j < 10; j++) {
-        document.write(j + " * " + i + " = " + i * j + " ");
-        document.write(" " + " " + " ");
-        // document.write("<br>");
+        document.write(i + " * " + j + " = " + i * j + " ");
+        document.writeln(" " + " " + " ");
+
     }
-    // document.write("<br>");
-    document.writeln();
+
+    document.writeln("</pre> </span> ");
 }
