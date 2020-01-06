83eac873b75e0ec69ee46a11a83858459e2b15f6
Simona Gorneanu <gorneanusimona@gmail.com>
2020-01-06 22:01:32 +0200

    ghiceste numarul

---
+++ teme JS/19.12.2019/what-is-number/number.js
@@ -0,0 +1,45 @@
+
+
+var nrCautat = Math.floor(Math.random() * 100);
+var incercariRamase = 7;
+var gasit = false;
+
+function incercari() {
+    var nr = document.getElementById("numar").value;
+
+    if (conditiiDeOprire() == false) {
+        return;
+    }
+    
+    verifica(nr);
+    incercariRamase--;
+    document.getElementById("nrIncercari").innerHTML = incercariRamase;
+
+}
+
+function conditiiDeOprire() {
+    if (gasit == true) {
+        return false;
+    }
+    if (incercariRamase == 0) {
+        return false;
+    }
+    return true;
+}
+
+function verifica(nr) {
+
+    if (nrCautat == nr) {
+        gasit = true;
+        document.getElementById("buton").style.backgroundColor = "green";
+
+        document.getElementById("verificare").innerHTML = "Ai ghicit!";
+
+    }
+    else if (nrCautat >= nr) {
+        document.getElementById("verificare").innerHTML = "Prea mic";
+    }
+    else {
+        document.getElementById("verificare").innerHTML = "Prea mare";
+    }
+}
