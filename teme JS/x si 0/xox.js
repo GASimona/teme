84c845ecad8a376c13d810034100e28a8c6fcd84
Simona Gorneanu <gorneanusimona@gmail.com>
2020-01-28 20:52:17 +0200

    click

---
+++ teme JS/x si 0/xox.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('td')
+        .forEach(cell => cell.addEventListener("click", onClick));
+
+var simbol = "X";
+
+function onClick(event) {
+    let cell = event.target;
+    cell.removeEventListener("click", onClick);
+    cell.innerText = simbol;
+    simbol = simbol == 'X' ? '0' : 'X'; 
+}
+
+// --- functie care verifica daca s-a terminat jocul ------
